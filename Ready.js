021e0317ce0854838b8ed6b6e06d615803867950
Shreyan Roy <royshreyan84@gmail.com>
2026-09-23 00:17:07 +0530

    Use discord.js ActivityType

--- Ready.js
+++ Ready.js
@@ -1,9 +1,11 @@
+const { ActivityType } = require('discord.js');
+
 module.exports = {
   name: 'ready',
   once: true,
   execute(client) {
     console.log(`✅ Logged in as ${client.user.tag}`);
     console.log(`📡 Serving ${client.guilds.cache.size} server(s)`);
-    client.user.setActivity('!help | Moderating 🔨', { type: 'WATCHING' });
+    client.user.setActivity('!help | Moderating 🔨', { type: ActivityType.Watching });
   },
 };
